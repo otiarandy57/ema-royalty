7355d7f860018380f3f07b5e42bc2371b84658aa
otiarandy57 <otiarandy679@gmail.com>
2025-07-31 03:46:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var MenuItems = document.getElementById("menu-items");
 MenuItems.style.maxHeight = "0px";
 
 function menutoggle(){
-    if(MenuItems.style.maxHeight == "0"){
+   if(MenuItems.style.maxHeight === "0px"){
         MenuItems.style.maxHeight = "200px";
     } else {
         MenuItems.style.maxHeight = "0px";
